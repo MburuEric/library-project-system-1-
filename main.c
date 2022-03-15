8b8252f90b662504764b899399164347fcd5a4d0
MburuEric <99777790+MburuEric@users.noreply.github.com>
2022-03-15 04:49:23 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+    project: county library management system.
+    Author:  Titus Njiru
+    Date:    Oct, 2021
+    Compiler: GNU GCC
+    Language: C99
+    License: MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        printf("\tCounty Library\n");
+        printf("Welcome Mr. Eric. \n");
+        printf("what would you like to do?\n");
+        printf("1. Add new User\n");
+        printf("2. Add book. \n");
+        printf("selected Action: ");
+        return 0;
+}
